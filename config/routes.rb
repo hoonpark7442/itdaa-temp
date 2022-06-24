56cae61bac0b25af9c7c07886cfcca8ef08f232b
Hoon Park <55367357+hoonpark7442@users.noreply.github.com>
2022-06-24 16:49:59 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # classes 말고 다른 클래스명 사용 필요
+  post "/itdaa_classes", to: "classes#create"
 end
